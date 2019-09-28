8ff4ec8caa140e554ca7b8d5871ec44163b61d2c
Ragagno <ragocraft@gmail.com>
2019-09-28 23:32:34 +0200

    + unsigned union type punning

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -1,1 +1,31 @@
 pub mod masks;
+
+pub union UPun8 {
+    u8: * u8,
+}
+
+pub union UPun16 {
+    u8: * u8,
+    u16: * u16,
+}
+
+pub union UPun32 {
+    u8: * u8,
+    u16: * u16,
+    u32: * u32,
+}
+
+pub union UPun64 {
+    u8: * u8,
+    u16: * u16,
+    u32: * u32,
+    u64: * u64,
+}
+
+pub union UPun128 {
+    u8: * u8,
+    u16: * u16,
+    u32: * u32,
+    u64: * u64,
+    u128: * u128,
+}
