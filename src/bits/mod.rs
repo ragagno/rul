97454c3263ccb44a08acce7ba1450114f9a8098b
Ragagno <ragocraft@gmail.com>
2019-09-29 00:09:29 +0200

    * const and mut UPun fix

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -2,32 +2,62 @@ pub mod masks;
 
 const BITS_PER_BYTE: usize = 8usize;
 
-pub union UPun8 {
-    u8: * u8,
+pub union UPunConst8 {
+    u8: *const u8,
 }
 
-pub union UPun16 {
-    u8: * u8,
-    u16: * u16,
+pub union UPunConst16 {
+    u8: *const u8,
+    u16: *const u16,
 }
 
-pub union UPun32 {
-    u8: * u8,
-    u16: * u16,
-    u32: * u32,
+pub union UPunConst32 {
+    u8: *const u8,
+    u16: *const u16,
+    u32: *const u32,
 }
 
-pub union UPun64 {
-    u8: * u8,
-    u16: * u16,
-    u32: * u32,
-    u64: * u64,
+pub union UPunConst64 {
+    u8: *const u8,
+    u16: *const u16,
+    u32: *const u32,
+    u64: *const u64,
 }
 
-pub union UPun128 {
-    u8: * u8,
-    u16: * u16,
-    u32: * u32,
-    u64: * u64,
-    u128: * u128,
+pub union UPunConst128 {
+    u8: *const u8,
+    u16: *const u16,
+    u32: *const u32,
+    u64: *const u64,
+    u128: *const u128,
+}
+
+pub union UPuMutn8 {
+    u8: *mut u8,
+}
+
+pub union UPunMut16 {
+    u8: *mut u8,
+    u16: *mut u16,
+}
+
+pub union UPunMut32 {
+    u8: *mut u8,
+    u16: *mut u16,
+    u32: *mut u32,
+}
+
+pub union UPunMut64 {
+    u8: *mut u8,
+    u16: *mut u16,
+    u32: *mut u32,
+    u64: *mut u64,
+}
+
+pub union UPun1Mut28 {
+    u8: *mut u8,
+    u16: *mut u16,
+    u32: *mut u32,
+    u64: *mut u64,
+    u128: *mut u128,
 }
