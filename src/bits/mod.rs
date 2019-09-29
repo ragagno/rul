8e2c88a2dd5591ab5e1524784fa8440632b309fc
Ragagno <ragocraft@gmail.com>
2019-09-29 02:10:11 +0200

    * visibility fixed for type punning unions

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -10,61 +10,61 @@ const BITS_PER_BYTE_U64: u64 = 8u64;
 const BITS_PER_BYTE_U128: u128 = 8u128;
 
 pub union UPunConst8 {
-    u8: *const u8,
+    pub u8: *const u8,
 }
 
 pub union UPunConst16 {
-    u8: *const u8,
-    u16: *const u16,
+    pub u8: *const u8,
+    pub u16: *const u16,
 }
 
 pub union UPunConst32 {
-    u8: *const u8,
-    u16: *const u16,
-    u32: *const u32,
+    pub u8: *const u8,
+    pub u16: *const u16,
+    pub u32: *const u32,
 }
 
 pub union UPunConst64 {
-    u8: *const u8,
-    u16: *const u16,
-    u32: *const u32,
-    u64: *const u64,
+    pub u8: *const u8,
+    pub u16: *const u16,
+    pub u32: *const u32,
+    pub u64: *const u64,
 }
 
 pub union UPunConst128 {
-    u8: *const u8,
-    u16: *const u16,
-    u32: *const u32,
-    u64: *const u64,
-    u128: *const u128,
+    pub u8: *const u8,
+    pub u16: *const u16,
+    pub u32: *const u32,
+    pub u64: *const u64,
+    pub u128: *const u128,
 }
 
-pub union UPuMutn8 {
-    u8: *mut u8,
+pub union UPunMut8 {
+    pub u8: *mut u8,
 }
 
 pub union UPunMut16 {
-    u8: *mut u8,
-    u16: *mut u16,
+    pub u8: *mut u8,
+    pub u16: *mut u16,
 }
 
 pub union UPunMut32 {
-    u8: *mut u8,
-    u16: *mut u16,
-    u32: *mut u32,
+    pub u8: *mut u8,
+    pub u16: *mut u16,
+    pub u32: *mut u32,
 }
 
 pub union UPunMut64 {
-    u8: *mut u8,
-    u16: *mut u16,
-    u32: *mut u32,
-    u64: *mut u64,
+    pub u8: *mut u8,
+    pub u16: *mut u16,
+    pub u32: *mut u32,
+    pub u64: *mut u64,
 }
 
 pub union UPun1Mut28 {
-    u8: *mut u8,
-    u16: *mut u16,
-    u32: *mut u32,
-    u64: *mut u64,
-    u128: *mut u128,
+    pub u8: *mut u8,
+    pub u16: *mut u16,
+    pub u32: *mut u32,
+    pub u64: *mut u64,
+    pub u128: *mut u128,
 }
