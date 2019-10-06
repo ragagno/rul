4f4bb456f98c9ae3048b8f603ec05b884b7870e6
Ragagno <ragocraft@gmail.com>
2019-10-06 22:48:22 +0200

    * export BITS_PER_BYTE constants

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -2,10 +2,10 @@ pub mod masks;
 pub mod endian;
 pub mod punning;
 
-const BITS_PER_BYTE: usize = 8usize;
+pub const BITS_PER_BYTE: usize = 8usize;
 
-const BITS_PER_BYTE_U8: u8 = 8u8;
-const BITS_PER_BYTE_U16: u16 = 8u16;
-const BITS_PER_BYTE_U32: u32 = 8u32;
-const BITS_PER_BYTE_U64: u64 = 8u64;
-const BITS_PER_BYTE_U128: u128 = 8u128;
+pub const BITS_PER_BYTE_U8: u8 = 8u8;
+pub const BITS_PER_BYTE_U16: u16 = 8u16;
+pub const BITS_PER_BYTE_U32: u32 = 8u32;
+pub const BITS_PER_BYTE_U64: u64 = 8u64;
+pub const BITS_PER_BYTE_U128: u128 = 8u128;
